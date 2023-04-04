8ab11a22b8b8540bc11a456775bbebc0ba0a343c
athi27 <120363755+athi27@users.noreply.github.com>
2023-04-04 10:33:21 +0530

    Add files via upload

---
+++ stringprgm.java
@@ -0,0 +1,18 @@
+class stringprgm{
+public static void main(String b[])
+{
+String s1="a";
+String s2="b";
+String s3=new String("SNGCE");
+System.out.println(s1);
+System.out.println(s2);
+System.out.println(s3);
+System.out.println(s1.length());
+System.out.println(s1+" "+s2);
+System.out.println(s1.concat(s2));
+System.out.println(s1.compareTo(s2)); 
+System.out.println(s1.substring(0,1));
+System.out.println(s1.equals(s2));
+System.out.println(s1.replace('a','o'));
+System.out.println(s1.toUpperCase());
+}}
